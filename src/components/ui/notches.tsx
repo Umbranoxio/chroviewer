8c756b2098e9658837fd5e6f800e3f8d510aaf66
Cubic <132150361+MrCubicYT@users.noreply.github.com>
2026-07-23 04:44:16 +0100

    cleanup dum code

--- src/components/ui/notches.tsx
+++ src/components/ui/notches.tsx
@@ -1,5 +1,3 @@
-import { useEffect, useState } from 'react';
-
 import { cn } from '@/lib/utils';
 
 interface NotchesProps {
@@ -12,31 +10,25 @@ interface NotchesProps {
 }
 
 function Notches({ orientation, min = 0, max = 100, step = 1, divisor = 1, value }: NotchesProps) {
-  const [localValues, setLocalValues] = useState(value);
-  useEffect(() => {
-    if (value) setLocalValues(value);
-  }, [value]);
-
   return (
     <>
       {Array.from({ length: Math.round((max - min) / step) + 1 }).map((_, index, notches) => {
         if (index % divisor !== 0) return null;
         const isBigBoyNotch = [0, notches.length - 1, Math.floor((notches.length - 1) / 2)].includes(index);
         const percentage = (index / (notches.length - 1)) * 100;
-        const isActive = localValues !== undefined ? localValues.some((v) => Math.abs(v - (min + index * step)) < step * 0.5) : false;
+
+        const currentNotch = min + index * step;
+        const isActive = value !== undefined ? value.some((v) => v == currentNotch) : false;
 
         return (
           <div
             key={index}
-            data-orientation={orientation}
-            style={orientation === 'vertical' ? { bottom: `${percentage}%` } : { left: `${percentage}%` }}
+            style={orientation === 'horizontal' ? { left: `${percentage}%` } : { bottom: `${percentage}%` }}
             className={cn(
               'absolute rounded-full shrink-0 transition-all duration-100',
+              orientation === 'horizontal' ? 'top-1/2 -translate-y-1/2 -translate-x-1/2 w-0.5' : 'left-1/2 -translate-x-1/2 translate-y-1/2 h-0.5',
               isBigBoyNotch && isActive ? 'bg-white' : 'bg-muted-foreground/20 z-10',
-              'data-[orientation=horizontal]:top-1/2 data-[orientation=horizontal]:-translate-y-1/2 data-[orientation=horizontal]:-translate-x-1/2 data-[orientation=horizontal]:w-0.5',
-              isActive ? 'data-[orientation=horizontal]:h-6' : isBigBoyNotch ? 'data-[orientation=horizontal]:h-4 bg-muted-foreground/20 z-10' : 'data-[orientation=horizontal]:h-2',
-              'data-[orientation=vertical]:left-1/2 data-[orientation=vertical]:-translate-x-1/2 data-[orientation=vertical]:translate-y-1/2 data-[orientation=vertical]:h-0.5',
-              isActive ? 'data-[orientation=vertical]:w-6' : isBigBoyNotch ? 'data-[orientation=vertical]:w-4 bg-muted-foreground/20 z-10' : 'data-[orientation=vertical]:w-2',
+              orientation === 'horizontal' ? (isActive ? 'h-6' : isBigBoyNotch ? 'h-4 bg-muted-foreground/20 z-10' : 'h-2') : isActive ? 'w-6' : isBigBoyNotch ? 'w-4 bg-muted-foreground/20 z-10' : 'w-2',
             )}
           />
         );
